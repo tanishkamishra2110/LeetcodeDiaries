af55b6a60485bc6067089ca4e0ffaf0c2c32d0e8
Tanishka Mishra <97809876+tanishkamishra2110@users.noreply.github.com>
2023-04-12 14:51:42 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (63.27%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int a=0;
+        int l=0;
+        for(int i=0; i<s.length();i++)
+        {
+            if(s[i]=='A')
+            { 
+                a++;
+            if(a>=2)
+            {
+                return false;
+            }
+            }
+            if(i<(s.length()-2))
+            {
+            if(s[i]=='L'&& s[i+1]=='L'&& s[i+2]=='L')
+            {   
+                return false;
+            }
+            }
+        }
+        return true;
+        
+        
+    }
+};
