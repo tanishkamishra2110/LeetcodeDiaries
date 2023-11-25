2a4d2ddb686aaf44ae8775866cca117ee0a792ef
Tanishka Mishra <97809876+tanishkamishra2110@users.noreply.github.com>
2023-11-25 23:16:12 +0530

    Time: 122 ms (10.51%), Space: 84.5 MB (16.56%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+          map<int, int> m;
+        int ans = 0, count = 0;
+        m[0] = -1;
+        for(int i=0;i<nums.size();i++) {
+            count += (nums[i] == 0) ? -1 : 1;
+            if(m.find(count) != m.end()) {
+                ans = max(ans, i-m[count]);
+            } else m[count] = i;
+        }
+        return ans;
+    }
+};
