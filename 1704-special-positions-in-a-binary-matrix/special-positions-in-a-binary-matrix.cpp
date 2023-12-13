348f59434d292b8fc12a07b751b0007f11a6f14a
Tanishka Mishra <97809876+tanishkamishra2110@users.noreply.github.com>
2023-12-13 23:23:54 +0530

    Time: 20 ms (34.47%) | Memory: 13 MB (99.47%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+          int m = mat.size(), n = mat[m-1].size();
+        int ans = 0;
+        for(int i=0;i<m;i++){
+            int oneCount = 0;
+            bool flag = false;
+            for(int j=0;j<n;j++){
+                if( mat[i][j] ){
+                    oneCount += 1;
+                    flag = flag | isValid(mat, j, m);
+                }
+            }
+            ans += oneCount==1 && flag? 1 : 0;
+        }
+        return ans;
+    }
+
+    bool isValid(vector<vector<int>>& mat, int colIdx, int &m){
+        int count = 0;
+        for(int i=0;i<m;i++){
+            count += mat[i][colIdx];
+        }
+        return count==1;
+    }
+};
