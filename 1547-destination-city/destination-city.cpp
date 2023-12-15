0d62b38887f9877e1cec2e025d831ffb71e61560
Tanishka Mishra <97809876+tanishkamishra2110@users.noreply.github.com>
2023-12-15 23:22:11 +0530

    Time: 14 ms (19.38%) | Memory: 11.1 MB (59.81%) - LeetSync

---
+++ 1547-destination-city/destination-city.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+   
+        map<string, bool> hmap;
+        for(auto &path : paths){
+            hmap[path[0]] = true;
+        }
+        for(auto &path : paths){
+            if( hmap.find(path[1]) == hmap.end() ){
+                return path[1];
+            }
+        }
+        return "";
+    }
+};
